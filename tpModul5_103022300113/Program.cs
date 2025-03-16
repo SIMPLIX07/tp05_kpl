c749a5a449b15b9e116ac96bf412fdea15f67b52
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-03-16 19:14:21 +0700

    Menambahkan class DataGeneric dengan method PrintData

--- tpModul5_103022300113/Program.cs
+++ tpModul5_103022300113/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpModul5_103022300113; 
+namespace jurnal
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            HaloGeneric hg = new HaloGeneric(); 
+            hg.SapaUser("John");
+
+            DataGeneric<string> data1 = new DataGeneric<string>("12345678");
+            data1.PrintData();  
+        }
+    }
+}
--- tpModul5_103022300113/obj/Debug/net8.0/tpModul5_103022300113.AssemblyInfo.cs
+++ tpModul5_103022300113/obj/Debug/net8.0/tpModul5_103022300113.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpModul5_103022300113")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e4d3246d1594dedac50d719d53068d5d2dbaa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpModul5_103022300113")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpModul5_103022300113")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
